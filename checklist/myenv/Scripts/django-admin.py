d2c5f30cf895601208c5a05ea3a877bd69a7cfe1
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2018-12-18 00:23:04 +0100

    updated gitignore

--- checklist/myenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\erykt\desktop\django\checklist\checklist\myenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
